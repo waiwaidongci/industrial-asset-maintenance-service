c658f2b09a22fb9698a77913140004fe154be68e
waiwaidongci <786958579@qq.com>
2026-08-24 20:58:45 +0800

    feat: initial import

---
+++ internal/platform/observability/metrics.go
@@ -0,0 +1,21 @@
+package observability
+
+import (
+	"fmt"
+	"net/http"
+	"sync/atomic"
+)
+
+type Metrics struct {
+	Requests atomic.Uint64
+	Errors   atomic.Uint64
+	Assets   atomic.Uint64
+	Plans    atomic.Uint64
+	Tasks    atomic.Uint64
+	Findings atomic.Uint64
+}
+
+func (m *Metrics) Render(w http.ResponseWriter) {
+	w.Header().Set("Content-Type", "text/plain; version=0.0.4")
+	fmt.Fprintf(w, "http_requests_total %d\nhttp_errors_total %d\nmaintenance_assets_total %d\nmaintenance_plans_total %d\nmaintenance_tasks_total %d\nmaintenance_findings_total %d\n", m.Requests.Load(), m.Errors.Load(), m.Assets.Load(), m.Plans.Load(), m.Tasks.Load(), m.Findings.Load())
+}
