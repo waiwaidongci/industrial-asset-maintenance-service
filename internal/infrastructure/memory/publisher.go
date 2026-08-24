76825342e8b5e59db0e7ca02870199c336c10469
waiwaidongci <786958579@qq.com>
2026-08-24 19:00:21 +0800

    feat: initial import

---
+++ internal/infrastructure/memory/publisher.go
@@ -0,0 +1,23 @@
+package memory
+
+import (
+	"context"
+	"github.com/example/asset-maintenance-service/internal/domain"
+	"sync"
+)
+
+type Publisher struct {
+	mu     sync.Mutex
+	Events []domain.Event
+}
+
+func NewPublisher() *Publisher { return &Publisher{Events: []domain.Event{}} }
+func (p *Publisher) Publish(ctx context.Context, e domain.Event) error {
+	if err := ctx.Err(); err != nil {
+		return err
+	}
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.Events = append(p.Events, e)
+	return nil
+}
